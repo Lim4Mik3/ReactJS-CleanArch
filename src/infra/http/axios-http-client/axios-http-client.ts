85963f82d993c94feea4081c984ff59b634b8931
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2022-02-09 05:48:37 -0300

    feat: ensure axios httpclient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import { HttpPostParams } from "@/data/protocols/http";
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async post({ url }: HttpPostParams<any>): Promise<void> {
+    await axios(url);
+  }
+}
